0c4935c9a9a1d349b8f3d1ab4acffcceb4376474
s1290140 <s1290140@u-aizu.ac.jp>
2022-07-14 14:25:35 +0900

    Victory message added

--- Heads_or_Tails.java
+++ Heads_or_Tails.java
@@ -1,4 +1,4 @@
-import java.util.Random;
+import java.util.*;
 
 public class Heads_or_Tails {
     public static void main(String[] args) {
@@ -6,6 +6,13 @@ public class Heads_or_Tails {
         Random r = new Random();
         int round = 1;
         int[] count = { 0, 0 };
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Who are you?");
+        System.out.print("> ");
+        String name = sc.next();
+        System.out.println("Hello, " + name + "!");
 
         System.out.println("Tossing a coin...");
         for (int i = 0; i < 3; i++) {
@@ -20,5 +27,10 @@ public class Heads_or_Tails {
         }
 
         System.out.println("Heads: " + count[0] + ", Tails: " + count[1]);
+
+        if (count[0] > count[1])
+            System.out.println("You won!");
+        else
+            System.out.println("You lost!");
     }
 }
